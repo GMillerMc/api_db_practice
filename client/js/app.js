2b8eb274c8ba03aa9c8617574c389cf24950a7fc
GMillerMc <gmiller04@yahoo.com>
2022-05-31 15:45:46 +0100

    update to appjs

--- client/js/app.js
+++ client/js/app.js
@@ -5,6 +5,13 @@ const feedDisplay = document.querySelector('#feed')
 
 fetch('http://localhost:3000/subscribers')
 .then(response => response.json())
-.then(data => console.log(data))
-
+.then(data => {
+    data.forEach(article => {
+    const articleItem = `<div>
+    <h3>` + article.title + `</h3>
+    <p>` + article.message + `</p></div>`
+    feedDisplay.insertAdjacentHTML('beforeend', articleItem)
+})
+})
+.catch(err => console.log(err))
 
